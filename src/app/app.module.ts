e9c2741dd5c6072bbe8449c4a33407c1e703010c
Oskin.С <constantoskin@gmail.com>
2020-06-10 12:47:04 +0300

    Add material theming

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,26 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NoopAnimationsModule } from '@angular/platform-browser/animations';
+import { NmThemingModule, NmTextFormFieldComponent } from '@imediasoftllc/angular-components';
+import { MatError, MatFormField, MatLabel } from '@angular/material';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NmTextFormFieldComponent,
+    MatFormField,
+    MatLabel,
+    MatError
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NoopAnimationsModule,
+    NmThemingModule,
+    FormsModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
